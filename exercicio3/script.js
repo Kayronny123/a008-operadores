e51ca7615a28df7e999da38b203c9dc735dcf823
rogerio kayronny <kayronnysales@gmail.com>
2022-07-21 09:02:50 -0300

    atualizado exercicio 3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -12,13 +12,13 @@
 // ```
 
 const op1 = (5 > 20 && 20 < 2);
-console.log(op1);
+console.log("5 é maior que 20 e também é menor que 2", op1);
 
 const op2 = (5 === 5 || 5 === "5");
-console.log(op2);
+console.log("5 é igual a 5 ou é igual a “5", op2);
 
 let op3 = (!20 > 50);
-console.log(op3);
+console.log("negação de vinte é maior que cinquenta", op3);
 
 let op4 = !(20 > 50);
-console.log(op4);
+console.log("negação de (vinte é maior que cinquenta)", op4);
