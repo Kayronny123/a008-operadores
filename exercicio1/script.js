e68f942029e94fc580ce575435a9f8f1d626406d
rogerio kayronny <kayronnysales@gmail.com>
2022-07-21 08:39:49 -0300

    finalizado exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+const numePar = prompt("insira um numero par")
+Resultado1 = numePar % 2
+console.log("resto é", Resultado1)
+
+const numePar1 = prompt("insira um numero par")
+Resultado2 = numePar1 % 2
+console.log("resto é", Resultado2)
+
+// o resto quanto insiro um numero par sempre será 0 pois qualquer valor par dividido por dois é 0
+// já quando insiro um numero impar o resto sempre é 1 desde que o divisor for 2
